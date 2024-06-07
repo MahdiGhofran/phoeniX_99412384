cafd777d667421e825c6ab23cdce5f119af78cce
Mahdi <mahdi.ghofran.dev@gmail.com>
2024-06-08 02:06:51 +0330

    Run test_1-100 code on phoeniX core

--- phoeniX_Testbench.v
+++ phoeniX_Testbench.v
@@ -2,7 +2,7 @@
 `include "phoeniX.v"
 
 `ifndef FIRMWARE
-    `define FIRMWARE "Software\\User_Codes\\test\\test_firmware.hex"
+    `define FIRMWARE "Software\\User_Codes\\test_1-100\\test_1-100_firmware.hex"
 `endif /*FIRMWARE*/
 
 `ifndef START_ADDRESS
